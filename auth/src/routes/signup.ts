1e28239b4f098e79562b2d42dad4077defe21be6
Lorenzo Faivre <lfaivre@asu.edu>
2020-06-03 18:22:26 -0700

    feat: add validation to auth route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,28 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('Hello World');
-});
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Invalid email.'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage(
+        'Invalid password. Password must be within 4 and 20 characters.'
+      ),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+    const { email, password } = req.body;
+    console.log('Creating a user...');
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
